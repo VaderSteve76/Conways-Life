28a63dcd879310dd4da1b7955601ea51dabe3410
VaderSteve76 <alverson.s@yahoo.com>
2019-07-15 19:06:18 -0400

    Main

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
       <h1>Game of Life</h1>
+      <Main />
     </div>
   );
 }
